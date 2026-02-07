3a29c3a63aaf38be5b726ef9578b27397c75a097
NEEL <neelkamalpunj@gmail.com>
2026-02-08 00:07:00 +0530

    routing complete

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data, setData] = useState([])
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
 
-    useEffect(() => {
-        fetch('https://api.github.com/users/neverneel')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            setData(data)
-        })
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/neverneel')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // }, [])
 
   return (
     <div className='text-center m-4 bg-gray-500 text-3xl text-white p-4'>
@@ -21,3 +23,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/neverneel')
+    return response.json()
+}
